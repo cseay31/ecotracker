bf6c99c6ed4ac7617a17966a0893094beb337efd
Connor Seay <seayc31@ensworth.com>
2026-09-05 21:33:30 +0000

    Update map styling to dark theme

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -343,7 +343,7 @@ export default function MapView({
         className="rounded-xl overflow-hidden z-0"
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=cb1_2sh6_1_dbddc1154c3584ee2750a645"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?key=cb1_2sh6_1_dbddc1154c3584ee2750a645"
           attribution="&copy; OpenStreetMap contributors &copy; CARTO"
           subdomains="abcd"
           maxZoom={20}
